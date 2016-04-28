2e87d71e4fc21539a5eee5c6261b4725a15bec10
Tyler Long <tyler4long@gmail.com>
2016-04-28 12:23:23 +0800

    Add readme and config-sample.json for the CSharp demo

--- csharp-nancy/csharp-nancy/Program.cs
+++ csharp-nancy/csharp-nancy/Program.cs
@@ -15,7 +15,7 @@ namespace csharp_nancy
     {
         static void Main(string[] args)
         {
-            var uri = new Uri("http://localhost:" + Environment.GetEnvironmentVariable("MY_APP_PORT"));
+            var uri = new Uri(string.Format("http://{0}:{1}", Config.Instance.AppHost, Config.Instance.AppPort));
 
             using (var host = new NancyHost(uri))
             {
@@ -33,36 +33,36 @@ namespace csharp_nancy
         private const string MyState = "myState";
 
         // must be static. Because every time there is new request, a new instance of this class is created.
-        static SDK rcsdk = new SDK(Environment.GetEnvironmentVariable("RC_APP_KEY"),
-            Environment.GetEnvironmentVariable("RC_APP_SECRET"),
-            Environment.GetEnvironmentVariable("RC_APP_SERVER_URL"),
+        static SDK rcsdk = new SDK(Config.Instance.AppKey,
+            Config.Instance.AppSecret,
+            Config.Instance.ServerUrl,
             "C# OAuth2 demo app", "1.0.0");
 
 
         private string AuthorizeUri(string state)
         {
-            var baseUrl = Environment.GetEnvironmentVariable("RC_APP_SERVER_URL") + "/restapi/oauth/authorize";
+            var baseUrl = Config.Instance.ServerUrl + "/restapi/oauth/authorize";
             var authUrl = string.Format("{0}?response_type=code&state={1}&redirect_uri={2}&client_id={3}",
                 baseUrl, Uri.EscapeUriString(state),
-                Uri.EscapeUriString(Environment.GetEnvironmentVariable("RC_APP_REDIRECT_URL")),
-                Uri.EscapeUriString(Environment.GetEnvironmentVariable("RC_APP_KEY")));
+                Uri.EscapeUriString(Config.Instance.RedirectUrl),
+                Uri.EscapeUriString(Config.Instance.AppKey));
             return authUrl;
         }
 
         private Dictionary<string, string> GetAuthData(string authCode)
         {
-            var client = new RestClient(Environment.GetEnvironmentVariable("RC_APP_SERVER_URL"));
+            var client = new RestClient(Config.Instance.ServerUrl);
             var request = new RestRequest("/restapi/oauth/token", Method.POST);
 
             request.AddHeader("Accept", "application/json");
             request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-            var apiKey = string.Format("{0}:{1}", Environment.GetEnvironmentVariable("RC_APP_KEY"),
-                Environment.GetEnvironmentVariable("RC_APP_SECRET"));
+            var apiKey = string.Format("{0}:{1}", Config.Instance.AppKey,
+                Config.Instance.AppSecret);
             apiKey = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(apiKey));
             request.AddHeader("Authorization", "Basic " + apiKey);
 
             request.AddParameter("grant_type", "authorization_code");
-            request.AddParameter("redirect_uri", Environment.GetEnvironmentVariable("RC_APP_REDIRECT_URL"));
+            request.AddParameter("redirect_uri", Config.Instance.RedirectUrl);
             request.AddParameter("code", authCode);
 
             var response = client.Execute(request);
@@ -73,7 +73,7 @@ namespace csharp_nancy
         public DefaultModule()
         {
             var authorizeUri = AuthorizeUri(MyState);
-            var redirect_uri = Environment.GetEnvironmentVariable("RC_APP_REDIRECT_URL");
+            var redirect_uri = Config.Instance.RedirectUrl;
             var template = File.ReadAllText("index.html");
             Get["/"] = _ =>
             {
