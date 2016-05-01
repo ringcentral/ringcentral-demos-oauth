282e297adb1a5440d45ec418433bd2722aa2ee65
John Wang <johncwang@gmail.com>
2016-05-01 12:14:43 -0700

    update comments

--- javascript-express/app.js
+++ javascript-express/app.js
@@ -9,9 +9,9 @@ app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
+// Start HTTP server
 var server = null;
 var port = process.env.MY_APP_PORT;
-
 var useTls = process.env.MY_APP_TLS_ENABLED > 0 ? true : false;
 
 if (useTls) {
@@ -30,6 +30,7 @@ if (useTls) {
   });
 }
 
+// Start RingCentral SDK
 var rcsdk = new ringcentral({
   server: process.env.RC_APP_SERVER_URL,
   appKey: process.env.RC_APP_KEY,
